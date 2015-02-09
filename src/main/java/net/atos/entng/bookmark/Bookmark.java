3e9f745e514240ef170234470d45201eca6e787e
dmtran <duy-minh.tran@atos.net>
2015-02-09 18:17:10 +0100

    #3109 Transition - delete bookmarks when users are deleted

--- src/main/java/net/atos/entng/bookmark/Bookmark.java
+++ src/main/java/net/atos/entng/bookmark/Bookmark.java
@@ -3,6 +3,7 @@ package net.atos.entng.bookmark;
 
 import net.atos.entng.bookmark.controllers.BookmarkController;
 import net.atos.entng.bookmark.filters.BookmarkOwnerOnly;
+import net.atos.entng.bookmark.services.BookmarkRepositoryEvents;
 
 import org.entcore.common.http.BaseServer;
 import org.entcore.common.mongodb.MongoDbConf;
@@ -14,6 +15,10 @@ public class Bookmark extends BaseServer {
 	@Override
 	public void start() {
 		super.start();
+
+		// Set RepositoryEvents implementation used to process events published for transition
+		setRepositoryEvents(new BookmarkRepositoryEvents());
+
 		addController(new BookmarkController(BOOKMARK_COLLECTION));
 		MongoDbConf.getInstance().setCollection(BOOKMARK_COLLECTION);
 		setDefaultResourceFilter(new BookmarkOwnerOnly());
---
+++ src/main/java/net/atos/entng/bookmark/services/BookmarkRepositoryEvents.java
@@ -0,0 +1,71 @@
+package net.atos.entng.bookmark.services;
+
+import static net.atos.entng.bookmark.Bookmark.BOOKMARK_COLLECTION;
+
+import org.entcore.common.mongodb.MongoDbResult;
+import org.entcore.common.user.RepositoryEvents;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.logging.Logger;
+import org.vertx.java.core.logging.impl.LoggerFactory;
+
+import com.mongodb.QueryBuilder;
+
+import fr.wseduc.mongodb.MongoDb;
+import fr.wseduc.mongodb.MongoQueryBuilder;
+import fr.wseduc.webutils.Either;
+
+public class BookmarkRepositoryEvents implements RepositoryEvents {
+
+	private static final Logger log = LoggerFactory.getLogger(BookmarkRepositoryEvents.class);
+	private final MongoDb mongo = MongoDb.getInstance();
+
+	@Override
+	public void exportResources(String exportId, String userId,
+			JsonArray groups, String exportPath, String locale) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void deleteGroups(JsonArray groups) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void deleteUsers(JsonArray users) {
+
+		if(users == null || users.size() == 0) {
+			log.warn("JsonArray users is null or empty");
+			return;
+		}
+
+		final String [] userIds = new String[users.size()];
+		for (int i = 0; i < users.size(); i++) {
+			JsonObject j = users.get(i);
+			userIds[i] = j.getString("id");
+		}
+
+		final JsonObject criteria = MongoQueryBuilder.build(QueryBuilder.start("owner.userId").in(userIds));
+
+		mongo.delete(BOOKMARK_COLLECTION, criteria,
+				MongoDbResult.validActionResultHandler(new Handler<Either<String,JsonObject>>() {
+					@Override
+					public void handle(Either<String, JsonObject> event) {
+						if(event.isLeft()) {
+							log.error("Error when deleting bookmarks : "+ event.left());
+						}
+						else {
+							String message = "Delete bookmarks successful : ";
+							if(event.right().getValue() != null) {
+								message += event.right().getValue().toString();
+							}
+							log.info(message);
+						}
+					}
+				})
+		);
+
+	}
+
+}
